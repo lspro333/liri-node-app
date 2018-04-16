1c6debb537257221f59d7998c622ad351c4192f6
Lisa Sprowls <lisasprowls@Lisas-MacBook-Pro.local>
2018-04-15 22:05:13 -0600

    updated files

--- liri.js
+++ liri.js
@@ -1,93 +1,155 @@
-var fs = require('fs');
+require("dotenv").config();
+
+//npm require
+var fs = require("fs");
+var keys = require("./keys.js");
 var Twitter = require('twitter');
-var spotify = require('spotify');
 var request = require('request');
-var keys = require('./keys.js');
-
-var app = {
-  "my-tweets": function() {
-    var client = new Twitter(keys);
-    client.get('statuses/user_timeline', function(error, tweetData, response) {
-      if (!error) {
-        console.log(' ');
-        console.log('My Tweets');
-        tweetData.forEach(function(obj) {
-          console.log('Time: ' + obj.created_at);
-          console.log('Tweet: ' + obj.text);
-        });
-      
-    
-
-        app.logData(tweetData);
-      } else {
-        console.log(error);
-      }
-    });
-  },
-  "spotify-this-song": function(keyword) {
-    spotify.search({ type: 'track', query: keyword || 'The Sign Ace of Base' }, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
-      }
-
-      if(data.tracks.items.length > 0) {
-        var record = data.tracks.items[0];
-
-        console.log('Song Info');
-        console.log('Artist: ' + record.artists[0].name);
-        console.log('Name: ' + record.name);
-        console.log('Link: ' + record.preview_url);
-        console.log('Album: ' + record.album.name);
+var Spotify = require('node-spotify-api');
 
 
-        app.logData(data);
-      } else {
-        console.log('No song data found.');
-      }
+//twitter npm specific call to get keys to twitter account
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var input = process.argv;
+var command = input[2];
+
+//request
+var name = "";
+for (i = 3; i < input.length; i++) {
+	name = name + " " + input[i];
+}
+
+name = name.trim().replace(" ", "+");
+if (command === "my-tweets") {
+	    		
+	var params = {screen_name: 'lspro333', limit: 20};
+	
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    for (var i = tweets.length - 1; i >= 0; i--) {
+	    	var myTweets = 
+	    		"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\r\n" + "\r\n" +
+	    		"Tweet Number: " + (tweets.length-i) + "\r\n" +
+	    		"Posted on: " + tweets[i].created_at + "\r\n" +
+	    		"Tweet Posted: " + tweets[i].text + "\r\n" + "\r\n" + "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	    	console.log(myTweets);
+	    	writeToLog(myTweets);
+			} 
+		}
+	})
+}
+
+else if (command === "spotify-this-song") {
+	if (name === "") {
+  		name = "The Sign"
+  	}
+	//same song info as above but looking at info for "The Sign" by Ace of Base.
+	spotify.search({ type: 'track', query: name, limit: 6 }, function(err, data) {
+ 	if (err) {
+    	return console.log('Error occurred: ' + err);
+  	}
+  	
+  	// console.log(JSON.stringify(data, null, 2));
+  	// console.log(data);
+  	var track = data.tracks.items[5];
+    var mySong =
+		"-----------------------------------------------------------------------" + "\r\n" +
+		"Song Title: " + name + "\r\n" +
+		"Artist: " + track.artists[0].name + "\r\n" +
+		"Album: " + track.album.name + "\r\n" + 
+		"Preview Link: " + track.preview_url + "\r\n" +
+		"-----------------------------------------------------------------------" + "\r\n"
+		console.log(mySong);
+		writeToLog(mySong);
+	})
+
+}
+
+else if (command === "movie-this") {
+ 	//If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+ 	if (name === "") {
+ 		name = "Mr. Nobody";
+ 	}
+
+	// Then run a request to the OMDB API with the movie specified
+	var queryUrl = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&apikey=trilogy";
 
 
+	request.get(queryUrl, function(error, response, body) {
+		
+    	// console.log(response);
+	  	if (!error && response.statusCode === 200) {
+	  		//var body = JSON.parse(body);
+	  		for (i = 0; i < JSON.parse(body).Ratings.length; i++) {
+	  			if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
+	  				tomatoesRating = JSON.parse(body).Ratings[i].Value;
+	  				// console.log(tomatoesRating);
+	  			}
+	  			if (JSON.parse(body).Ratings[i].Source === "Internet Movie Database") {
+	  				internetRating = JSON.parse(body).Ratings[i].Value;
+	  				// console.log(internetRating);
+	  			}
+	  		}
+	  		// Display Title of the movie, Year the movie came out, IMDB Rating of the movie, Rotten Tomatoes Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie.
+	  		var myMovie =
+  			"-----------------------------------------------------------------------" + "\r\n" +
+    		"Movie Title: " + JSON.parse(body).Title + "\r\n" +
+    		"Year movie released: " + JSON.parse(body).Year + "\r\n" +
+    		"Movie rating: " + JSON.parse(body).Rated + "\r\n" + 
+    		"Rotten Tomatoes Rating: " + tomatoesRating + "\r\n" +
+    		"Internet Movie Database Rating: " + internetRating + "\r\n" +
+    		"Country: " + JSON.parse(body).Country + "\r\n" + 
+    		"Language: " + JSON.parse(body).Language + "\r\n" + 
+    		"Movie Plot: " + JSON.parse(body).Plot + "\r\n" +
+    		 "-----------------------------------------------------------------------" + "\r\n"
+    		console.log(myMovie);
+    		writeToLog(myMovie);
 
-    });
-  },
-  "movie-this": function(query) {
-    request('http://www.omdbapi.com/?t=' + (query || 'Mr.Nobody') +'&tomatoes=true', function (error, response, info) {
-      if (!error && response.statusCode == 200) {
+  		}
+	});
 
-        var movieData = JSON.parse(info);
+}
 
-        console.log('Movie Info');
-        console.log('Title: ' + movieData.Title);
-        console.log('Year: ' + movieData.Year);
-        console.log('IMDB Rating: ' + movieData.imdbRating);
-        console.log('Country: ' + movieData.Country);
-        console.log('Language: ' + movieData.Language);
-        console.log('Plot: ' + movieData.Plot);
-        console.log('Actors: ' + movieData.Actors);
-        console.log('Rotten Tomatoes Rating: ' + movieData.tomatoRating);
-        console.log('Rotten Tomatoes URL: ' + movieData.tomatoURL);
+if (command === "do-what-it-says") {
+	// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+	// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+	// Feel free to change the text in that document to test out the feature for other commands.
+	fs.readFile("random.txt", "utf8", function(error, data) {
 
-        app.logData(movieData);
-      }
-    });
-  },
-  "do-what-it-says": function() {
-    fs.readFile('random.txt', 'utf8', function(err, data) {
-      if(err) throw err;
-      console.log(data.toString());
+  // If the code experiences any errors it will log the error to the console.
+		if (error) {
+			return console.log(error);
+		}
+		// console.log(data);
+		// Then split it by commas (to make it more readable)
+		var nameArr = data.split(",");
+		// // We will then re-display the content as an array for later use.
+		// console.log(nameArr);
 
-      var cmds = data.toString().split(',');
+		name = nameArr[1]
+		// console.log(name);
 
-      app[cmds[0].trim()](cmds[1].trim());
-    });
-  },
-  logData: function(data) {
-    fs.appendFile('log.txt', JSON.stringify(data, null, 2) + '\n', function(err) {
-      if(err) {
-        console.log(err);
-      }
-    });
-  }
-};
+		spotify.search({ type: 'track', query: name, limit: 1 }, function(err, data) {
+			if (err) {
+				return console.log('Error occurred: ' + err);
+			}
+			
+			// console.log(JSON.stringify(data, null, 2));
+			// console.log(data);
+			var track = data.tracks.items[0];
+			var randomSong =
+				"-----------------------------------------------------------------------" + "\r\n" +
+				"Song Title: " + name + "\r\n" +
+				"Artist: " + track.artists[0].name + "\r\n" +
+				"Album: " + track.album.name + "\r\n" + 
+				"Preview Link: " + track.preview_url + "\r\n" +
+				"-----------------------------------------------------------------------" + "\r\n"
+			console.log(randomSong);
+			writeToLog(randomSong);
+		})
+
+	});
+}
 
 
